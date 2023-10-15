2ce30522097691d463fe87983e79d500ccdca56a
Mehdi <mehdi@MacBook-Pro-de-Mehdi.local>
2023-10-15 17:32:58 +0200

    update proxies

--- packages/hardhat/hardhat.config.ts
+++ packages/hardhat/hardhat.config.ts
@@ -10,23 +10,26 @@ import "@typechain/hardhat";
 import "hardhat-gas-reporter";
 import "hardhat-contract-sizer";
 import "solidity-coverage";
+import { baseURIs, proxies, TablelandNetworkConfig } from "./network";
 
 dotenv.config();
 
 
 const config: HardhatUserConfig = {
-  solidity : {
-    version : "0.8.19",
+  solidity: {
+    version: "0.8.19",
     settings: {
       optimizer: {
         enabled: true,
         runs: 9999999,
-      }
-    }
-  },
-  gasReporter: {
-    enabled: process.env.REPORT_GAS !== undefined,
-    currency: "USD",
+      },
+      metadata: {
+        // do not include the metadata hash, since this is machine dependent
+        // and we want all generated code to be deterministic
+        // https://docs.soliditylang.org/en/v0.7.6/metadata.html
+        bytecodeHash: "none",
+      },
+    },
   },
   contractSizer: {
     alphaSort: true,
@@ -35,18 +38,25 @@ const config: HardhatUserConfig = {
     strict: true,
     only: [],
   },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: "USD",
+  },
   etherscan: {
-    apiKey : {
-      // ethereum 
+    apiKey: {
+      // ethereum
+      mainnet: process.env.ETHERSCAN_API_KEY || "",
       sepolia: process.env.ETHERSCAN_API_KEY || "",
 
-      // polygon 
+      // polygon
+      polygon: process.env.POLYSCAN_API_KEY || "",
       polygonMumbai: process.env.POLYSCAN_API_KEY || "",
-
     }
   },
   networks: {
-    sepolia : {
+   
+    // testnets
+    sepolia: {
       url: `https://eth-sepolia.g.alchemy.com/v2/${
         process.env.ETHEREUM_SEPOLIA_API_KEY ?? ""
       }`,
@@ -54,7 +64,8 @@ const config: HardhatUserConfig = {
         process.env.ETHEREUM_SEPOLIA_PRIVATE_KEY !== undefined
           ? [process.env.ETHEREUM_SEPOLIA_PRIVATE_KEY]
           : [],
-    }, 
+    },
+    
     mumbai: {
       url: `https://polygon-mumbai.g.alchemy.com/v2/${
         process.env.POLYGON_MUMBAI_API_KEY ?? ""
@@ -63,10 +74,45 @@ const config: HardhatUserConfig = {
         process.env.POLYGON_MUMBAI_PRIVATE_KEY !== undefined
           ? [process.env.POLYGON_MUMBAI_PRIVATE_KEY]
           : [],
+    },
+    "filecoin-calibration": {
+      url: `https://api.calibration.node.glif.io/rpc/v1${
+        process.env.FILECOIN_CALIBRATION_API_KEY ?? ""
+      }`,
+      accounts:
+        process.env.FILECOIN_CALIBRATION_PRIVATE_KEY !== undefined
+          ? [process.env.FILECOIN_CALIBRATION_PRIVATE_KEY]
+          : [],
     }
+  },
+  baseURIs,
+  proxies,
+};
 
-  }};
+declare module "hardhat/types/config" {
+  // eslint-disable-next-line no-unused-vars
+  interface HardhatUserConfig {
+    baseURIs: TablelandNetworkConfig;
+    proxies: TablelandNetworkConfig;
+  }
+}
 
-  export default config; 
+declare module "hardhat/types/runtime" {
+  // eslint-disable-next-line no-unused-vars
+  interface HardhatRuntimeEnvironment {
+    baseURI: string;
+    proxy: string;
+  }
+}
 
-  
+extendEnvironment((hre: HardhatRuntimeEnvironment) => {
+  // Get base URI for user-selected network
+  const uris = hre.userConfig.baseURIs as any;
+  hre.baseURI = uris[hre.network.name];
+
+  // Get proxy address for user-selected network
+  const proxies = hre.userConfig.proxies as any;
+  hre.proxy = proxies[hre.network.name];
+});
+
+export default config;
---
+++ packages/hardhat/network.ts
@@ -0,0 +1,24 @@
+export interface TablelandNetworkConfig {
+    // tableland testnet testnets
+    sepolia: string;
+    maticmum: string;
+    "filecoin-calibration": string;
+  }
+  
+  const homesteadAddr = "0x012969f7e3439a9B04025b5a049EB9BAD82A8C12";
+  
+  export const proxies: TablelandNetworkConfig = {
+    sepolia: "0xc50C62498448ACc8dBdE43DA77f8D5D2E2c7597D",
+    maticmum: "0x4b48841d4b32C4650E4ABc117A03FE8B51f38F68",
+    "filecoin-calibration": "0x030BCf3D50cad04c2e57391B12740982A9308621",
+  };
+  
+  const localTablelandURI = "http://localhost:8080/api/v1/tables/31337/";
+  export const baseURIs: TablelandNetworkConfig = {
+    // testnets
+    sepolia: "https://testnets.tableland.network/api/v1/tables/11155111/",
+    maticmum: "https://testnets.tableland.network/api/v1/tables/80001/",
+    "filecoin-calibration":
+      "https://testnets.tableland.network/api/v1/tables/314159/",
+  };
+  
