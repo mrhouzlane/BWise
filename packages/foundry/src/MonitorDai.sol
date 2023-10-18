fd1143a64d329b046e38fcaa206bd738475820e2
Mehdi <mehdi@MacBook-Pro-de-Mehdi.local>
2023-10-18 04:58:02 +0200

    feat: update

--- packages/foundry/src/MonitorDai.sol
+++ packages/foundry/src/MonitorDai.sol
@@ -34,29 +34,27 @@ contract MonitorDAI {
 
     function calculateAmountToDeposit() external view returns (uint256) {}
 
+    function requireRebalancing() external returns (bool) {
+        // chainlink retrieve ETH price in USD
+        int256 currentPrice = currencyPrice.getETHPrice(1);
+        require(currentPrice > 0, "Price must be greater than zero.");
+        // Calculate the percentage change from the last snapshot
+        int256 priceChange = int256(currentPrice * 100) / int256(1230 * 100000000) - 100;
+        
+        // Check if the absolute price change is greater than or equal to 3%
+        if (priceChange >= int256(threshold) || priceChange <= int256(-threshold)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 
     function deposit(uint256 amount) external {
+        require(this.requireRebalancing(), "Rebalancing is not required.");
         uint256 shares = savingsDai.deposit(amount, poolAddress);
         depositSnapshot[msg.sender] = lastSnapshotDeposit;
     }
 
-
-    function requireRebalancing() external view returns (bool) {
-
-        // chainlink retrieve ETH price in USD
-        int256 currentPrice = currencyPrice.getETHPrice(1);
-        require(currentPrice > 0, "Price must be greater than zero.");
-
-        // Calculate the percentage change from the last snapshot
-        int256 priceChange = int256(currentPrice * 100) / int256(lastPriceSnapshot) - 100;
-        
-        // Check if the absolute price change is greater than or equal to 3%
-        require(
-            priceChange >= int256(-threshold) && priceChange <= int256(threshold),
-            "Price variation exceeds 3%."
-        );
-
-        return true;
-    }
 
 }
